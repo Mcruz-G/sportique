ce8cf18afeb87256d5b53658865ed0ee3b48c5b5
Mcruz-G <macruzgom@gmail.com>
2022-01-15 23:49:21 -0600

    .

--- webapp/date_input.py
+++
@@ -1,8 +0,0 @@
-from datetime import date, datetime
-import streamlit as st
-
-def build_date_input():
-    date_input = st.date_input(
-     "Select the game's date",
-     datetime.now())
-    return date_input
